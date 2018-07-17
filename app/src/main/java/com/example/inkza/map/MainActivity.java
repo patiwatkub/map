107029f0bf78118c2197ae3ea3838113a2b92a0e
patiwat chumakliam <prawsiam10@gmail.com>
2018-07-17 11:42:41 +0700

    Test Commit

--- app/src/main/java/com/example/inkza/map/MainActivity.java
+++ app/src/main/java/com/example/inkza/map/MainActivity.java
@@ -26,16 +26,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void init(){
-        Button btnMap = (Button) findViewById(R.id.btnMap);
-        btnMap.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, MapActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
 
     public boolean isServicesOK(){
         Log.d(TAG, "isServicesOK: checking google services version");
